60f4b16c756159756a89d812b2564c91bba6b987
Nick <speedyleion@users.noreply.github.com>
2026-01-30 20:38:30 -0800

    Major fixup.
    Adding more comments

--- MouseSensor.cpp
+++ MouseSensor.cpp
@@ -3,7 +3,9 @@
 #include <SPI.h>
 
 
+// Based on https://www.espruino.com/datasheets/ADNS5050.pdf
 const int tWakeup = 55;
+
 // This time was re-used from capture taken for OEM EX-G initializing
 // PMW3320DB-TYDU
 const int tPowerUpCs = 2;
@@ -26,6 +28,7 @@ const int tWus = 22;
 
 const int IDLE_READ = 0x00;
 
+// Register addresses from https://www.epsglobal.com/Media-Library/EPSGlobal/Products/files/pixart/PMW3320DB-TYDU.pdf
 const int PROD_ID = 0x00;
 const int POWER_UP_RESET = 0x3A;
 const int PERFORMANCE = 0x22;
@@ -36,8 +39,10 @@ const int MOTION = 0x02;
 const int DELTA_X = 0x03;
 const int DELTA_Y = 0x04;
 
+// As specified in https://www.epsglobal.com/Media-Library/EPSGlobal/Products/files/pixart/PMW3320DB-TYDU.pdf
 const int MAX_DPI = 3500;
 const int DPI_RESOLUTION = 250;
+const int MAX_CLOCK_SPEED = 1_000_000;
 
 static uint8_t clampDpi(uint16_t dpi){
     if (dpi < DPI_RESOLUTION) {
@@ -51,7 +56,7 @@ static uint8_t clampDpi(uint16_t dpi){
 
 MouseSensor::MouseSensor(int8_t cs, uint16_t dpi, int8_t sck, int8_t cipo, int8_t copi) {
     SPI.begin(sck, cipo, copi);
-    _settings = SPISettings(1000000, SPI_MSBFIRST, SPI_MODE3);
+    _settings = SPISettings(MAX_CLOCK_SPEED, SPI_MSBFIRST, SPI_MODE3);
     _cs = cs;
     _dpi = clampDpi(dpi);
 
@@ -71,8 +76,11 @@ void MouseSensor::initPmw() {
   delay(tWakeup);
 
   write(POWER_UP_RESET, 0x5A);
+  // The OEM software read this value, copying the behavior to be safe
   read(PROD_ID);
   write(PERFORMANCE,0x80);
+  // These registers are unknown. They were observed to be written to by the OEM
+  // EX-G software, https://speedyleion.github.io/mice/electronics/2026/01/11/ex-g-pmw3320db-tydu-spi-traffic.html
   write(0x1D,	0x0A);
   write(0x14,	0x40);
   write(0x18,	0x40);
@@ -83,10 +91,22 @@ void MouseSensor::initPmw() {
   write(0x67,	0x26);
   write(0x21,	0x04);
   write(PERFORMANCE, 0x00);
+  // The resolution for the PMW3320DB-TYDU is documented as a max of 3500 DPI
+  // with a 250 DPI resolution. Observing the OEM EX-G software a value of 0x83
+  // was sent for 750 DPI, and a value of 0x86 was sent for a value of 1500 DPI
+  // It seems that the MSB needs to be set, and that the LSB's represent the DPI
+  // value. 3500/250 = 14 or 0x0D so this is likely 0x81-0x8D
   write(RESOLUTION,	0x80 | _dpi);
+  // The OEM software read the value before writing, copying the behavior to be
+  // safe
   read(AXIS_CONTROL);
+  // The 0XA0 value was observed from the OEM EX-G software. It's likely
+  // specific to the physical orientation of the sensor in the case.
   write(AXIS_CONTROL,	0xA0);
-  write(BURST_READ_FIRST,	0x03);
+  write(BURST_READ_FIRST,	0x02);
+
+  // The OEM software read these. I'm thinking it's likely to ensure they're
+  // cleared.
   read(MOTION);
   read(DELTA_X);
   read(DELTA_Y);
