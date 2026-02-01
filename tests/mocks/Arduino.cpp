b8ac212ade9e6efc231fa8a4f9f1d5ca66cdf838
Nick <speedyleion@users.noreply.github.com>
2026-01-31 18:48:14 -0800

    Add tests for read and write

--- tests/mocks/Arduino.cpp
+++ tests/mocks/Arduino.cpp
@@ -1,6 +1,8 @@
 #include "Arduino.h"
 
+ArduinoMock Arduino;
+
 void pinMode(int, int) {}
-void digitalWrite(int, int) {}
+void digitalWrite(int pin, int value) { Arduino.digitalWrite(pin, value); }
 void delay(int) {}
 void delayMicroseconds(int) {}
--- tests/mocks/Arduino.h
+++ tests/mocks/Arduino.h
@@ -1,11 +1,38 @@
 #ifndef ARDUINO_H_MOCK
 #define ARDUINO_H_MOCK
 
-#include <cstdint>
+#include <ostream>
+#include <vector>
 
 #define OUTPUT 1
 #define HIGH 1
 #define LOW 0
+
+struct GpioEvent {
+  int pin;
+  int value;
+
+  bool operator==(const GpioEvent &other) const {
+    return pin == other.pin && value == other.value;
+  }
+
+  friend std::ostream &operator<<(std::ostream &os, const GpioEvent &event) {
+    return os << "{pin=" << event.pin << ", "
+              << (event.value == HIGH ? "HIGH" : "LOW") << "}";
+  }
+};
+
+class ArduinoMock {
+public:
+  void digitalWrite(int pin, int value) { _gpioEvents.push_back({pin, value}); }
+  void clearEvents() { _gpioEvents.clear(); }
+  const std::vector<GpioEvent> &getGpioEvents() const { return _gpioEvents; }
+
+private:
+  std::vector<GpioEvent> _gpioEvents;
+};
+
+extern ArduinoMock Arduino;
 
 void pinMode(int pin, int mode);
 void digitalWrite(int pin, int value);
