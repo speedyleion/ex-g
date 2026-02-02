44ae19fe5dd224fbdd6d32638063ce18f7d4b6ae
Nick <speedyleion@users.noreply.github.com>
2026-02-01 19:26:36 -0800

    Add initial scroll wheel support

---
+++ ScrollWheel.hpp
@@ -0,0 +1,46 @@
+#ifndef SCROLL_WHEEL_HPP
+#define SCROLL_WHEEL_HPP
+#include <Arduino.h>
+#include <ESP32Encoder.h>
+#include <optional>
+
+class ScrollWheel {
+public:
+  /**
+   * @brief Construct a ScrollWheel that uses 2 signal pins
+   *
+   * Sets the 2 signal pins as inputs with pull-up resistors
+   *
+   * @param a The first signal pin of the scroll wheel
+   * @param b The second signal pin of the scroll wheel
+   */
+  ScrollWheel(uint8_t a, uint8_t b) {
+    ESP32Encoder::useInternalWeakPullResistors = puType::up;
+    _encoder = ESP32Encoder();
+    _encoder.attachHalfQuad((int)a, (int)b);
+  }
+
+  ScrollWheel(const ScrollWheel &) = delete;
+  ScrollWheel &operator=(const ScrollWheel &) = delete;
+
+  /**
+   * @brief Get the scroll delta since the last read.
+   *
+   * Returns the accumulated encoder count and resets it to zero.
+   *
+   * @return The scroll delta, or std::nullopt if no movement occurred.
+   */
+  std::optional<int> delta() {
+    auto count = _encoder.getCount();
+    if (count != 0) {
+      _encoder.clearCount();
+      return (int)count;
+    }
+    return std::nullopt;
+  }
+
+private:
+  ESP32Encoder _encoder;
+};
+
+#endif // SCROLL_WHEEL_HPP
